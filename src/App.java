0b61f81d2786ed88cbec6a72f2c139350e3e53b2
Zhoss <joss.90@mail.ru>
2022-09-14 15:41:33 +1000

    Курсовая работа (базовая + повышенная сложности)

--- src/App.java
+++ src/App.java
@@ -1,57 +1,7 @@
 public class App {
 
-
     public static void main(String[] args) {
-        Employee[] employeeRepository = new Employee[10];
-        employeeRepository[0] = new Employee("Друг Марат Григорьевич", 5, 100_000);
-        employeeRepository[1] = new Employee("Аглеев Данил Янович", 4, 72_000);
-        employeeRepository[2] = new Employee("Баймулдина Гульнара Яновна", 2, 85_000);
-        employeeRepository[3] = new Employee("Ахметгалин Григорий Васильевич", 3, 53_000);
-        employeeRepository[4] = new Employee("Ивченкова Ангелина Вячеславовна", 1, 66_000);
-        employeeRepository[5] = new Employee("Алабин Владислав Егорович", 1, 48_000);
-        employeeRepository[6] = new Employee("Даргомыжская Антонина Никитовна", 4, 97_000);
-        employeeRepository[7] = new Employee("Голубовская Карина Григорьевна", 3, 112_000);
-        employeeRepository[8] = new Employee("Садаков Григорий Валентинович", 5, 83_000);
-        employeeRepository[9] = new Employee("Хоробритова Татьяна Маратовна", 2, 70_000);
-
-        DataWorking.showAllData(employeeRepository);
-        System.out.println("");
-
-        DataWorking.calculateSalary(employeeRepository);
-        System.out.println("Общие затраты на зарплату - " + DataWorking.calculateSalary(employeeRepository) + " руб.");
-        System.out.println("");
-
-        DataWorking.findMinSalary(employeeRepository);
-        System.out.println("");
-
-        DataWorking.findMaxSalary(employeeRepository);
-        System.out.println("");
-
-        DataWorking.calculateAverageSalary(employeeRepository);
-        System.out.println("Средняя зарплата - " + DataWorking.calculateAverageSalary(employeeRepository) + " руб.");
-        System.out.println("");
-
-        DataWorking.findAllFullNames(employeeRepository);
-        System.out.println("");
-
-        int indexPercent = 11;
-        DataWorking.indexSalary(employeeRepository, indexPercent);
-        System.out.println("");
-
-        int departmentNumber = 5;
-        DataWorking.findMinSalaryInDepartment(employeeRepository, departmentNumber);
-        DataWorking.findMaxSalaryInDepartment(employeeRepository, departmentNumber);
-        DataWorking.findTotalSalaryInDepartment(employeeRepository, departmentNumber);
-        DataWorking.findAverageSalaryInDepartment(employeeRepository, departmentNumber);
-        int indexPercentInDepartment = 15;
-        DataWorking.indexSalaryInDepartment(employeeRepository, departmentNumber, indexPercentInDepartment);
-        DataWorking.printEmployeesOfDepartment(employeeRepository, departmentNumber);
-        System.out.println("");
-
-        int salaryLimit = 90_000;
-        DataWorking.findSalaryMoreThanSalaryLimit(employeeRepository, salaryLimit);
-        DataWorking.findSalaryLessThanSalaryLimit(employeeRepository, salaryLimit);
-        System.out.println("");
+        EmployeeBook employeeBook = new EmployeeBook();
 
     }
 }
--- src/EmployeeBook.java
+++ src/EmployeeBook.java
@@ -1,13 +1,69 @@
 import java.util.ArrayList;
 
-public class DataWorking {
-    public static void showAllData(Employee[] employees) {
+class EmployeeBook {
+    private final Employee[] employees;
+    public EmployeeBook() {
+        this.employees = new Employee[10];
+    }
+
+//        employeeRepository[0] = new EmployeeBook("Друг Марат Григорьевич", 5, 100_000);
+//        employeeRepository[1] = new Employee("Аглеев Данил Янович", 4, 72_000);
+//        employeeRepository[2] = new Employee("Баймулдина Гульнара Яновна", 2, 85_000);
+//        employeeRepository[3] = new Employee("Ахметгалин Григорий Васильевич", 3, 53_000);
+//        employeeRepository[4] = new Employee("Ивченкова Ангелина Вячеславовна", 1, 66_000);
+//        employeeRepository[5] = new Employee("Алабин Владислав Егорович", 1, 48_000);
+//        employeeRepository[6] = new Employee("Даргомыжская Антонина Никитовна", 4, 97_000);
+//        employeeRepository[7] = new Employee("Голубовская Карина Григорьевна", 3, 112_000);
+//        employeeRepository[8] = new Employee("Садаков Григорий Валентинович", 5, 83_000);
+//        employeeRepository[9] = new Employee("Хоробритова Татьяна Маратовна", 2, 70_000);
+//
+//        employeeRepository.showAllData();
+//        System.out.println("");
+//
+//        calculateSalary(employeeRepository);
+//        System.out.println("Общие затраты на зарплату - " + calculateSalary(employeeRepository) + " руб.");
+//        System.out.println("");
+//
+//        findMinSalary(employeeRepository);
+//        System.out.println("");
+//
+//        findMaxSalary(employeeRepository);
+//        System.out.println("");
+//
+//        calculateAverageSalary(employeeRepository);
+//        System.out.println("Средняя зарплата - " + calculateAverageSalary(employeeRepository) + " руб.");
+//        System.out.println("");
+//
+//        findAllFullNames(employeeRepository);
+//        System.out.println("");
+//
+//        int indexPercent = 11;
+//        indexSalary(employeeRepository, indexPercent);
+//        System.out.println("");
+//
+//        int departmentNumber = 5;
+//        findMinSalaryInDepartment(employeeRepository, departmentNumber);
+//        findMaxSalaryInDepartment(employeeRepository, departmentNumber);
+//        findTotalSalaryInDepartment(employeeRepository, departmentNumber);
+//        findAverageSalaryInDepartment(employeeRepository, departmentNumber);
+//        int indexPercentInDepartment = 15;
+//        indexSalaryInDepartment(employeeRepository, departmentNumber, indexPercentInDepartment);
+//        printEmployeesOfDepartment(employeeRepository, departmentNumber);
+//        System.out.println("");
+//
+//        int salaryLimit = 90_000;
+//        findSalaryMoreThanSalaryLimit(employeeRepository, salaryLimit);
+//        findSalaryLessThanSalaryLimit(employeeRepository, salaryLimit);
+//        System.out.println("");
+
+
+    public void showAllData() {
         for (Employee employee : employees) {
             System.out.println(employee);
         }
     }
 
-    public static int calculateSalary(Employee[] employees) {
+    public int calculateSalary(Employee[] employees) {
         int sum = 0;
         for (Employee employee : employees) {
             sum = sum + employee.getEmployeeSalary();
@@ -15,7 +71,7 @@ public class DataWorking {
         return sum;
     }
 
-    public static void findMinSalary(Employee[] employees) {
+    public void findMinSalary(Employee[] employees) {
         int min = employees[0].getEmployeeSalary();
         String minSalaryEmployee = employees[0].getEmployeeFullName();
         for (Employee employee : employees) {
@@ -27,7 +83,7 @@ public class DataWorking {
         System.out.println("Минимальная зарплата: " + min + " руб. Сотрудник - " + minSalaryEmployee);
     }
 
-    public static void findMaxSalary(Employee[] employees) {
+    public void findMaxSalary(Employee[] employees) {
         int max = employees[0].getEmployeeSalary();
         String maxSalaryEmployee = employees[0].getEmployeeFullName();
         for (Employee employee : employees) {
@@ -39,17 +95,17 @@ public class DataWorking {
         System.out.println("Максимальная зарплата: " + max + " руб. Сотрудник - " + maxSalaryEmployee);
     }
 
-    public static int calculateAverageSalary(Employee[] employees) {
+    public int calculateAverageSalary(Employee[] employees) {
         return calculateSalary(employees) / employees.length;
     }
 
-    public static void findAllFullNames(Employee[] employees) {
+    public void findAllFullNames(Employee[] employees) {
         for (Employee employee : employees) {
             System.out.println("Ф.И.О. - " + employee.getEmployeeFullName());
         }
     }
 
-    public static void indexSalary(Employee[] employees, int indexPercent) {
+    public void indexSalary(Employee[] employees, int indexPercent) {
         int increaseSalary;
         for (int i = 0; i < employees.length; i++) {
             increaseSalary = employees[i].getEmployeeSalary() * (100 + indexPercent) / 100;
@@ -57,7 +113,7 @@ public class DataWorking {
         }
     }
 
-    public static void findMinSalaryInDepartment(Employee[] employees, int departmentNumber) {
+    public void findMinSalaryInDepartment(Employee[] employees, int departmentNumber) {
         ArrayList<Employee> tmpList = new ArrayList<>();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getDepartmentNumber() == departmentNumber) {
@@ -76,7 +132,7 @@ public class DataWorking {
 
     }
 
-    public static void findMaxSalaryInDepartment(Employee[] employees, int departmentNumber) {
+    public void findMaxSalaryInDepartment(Employee[] employees, int departmentNumber) {
         ArrayList<Employee> tmpList = new ArrayList<>();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getDepartmentNumber() == departmentNumber) {
@@ -94,7 +150,7 @@ public class DataWorking {
         System.out.println("Максимальная зарплата в отделе " + departmentNumber + " - " + max + " руб. Сотрудник - " + maxSalaryEmployee);
     }
 
-    public static void findTotalSalaryInDepartment(Employee[] employees, int departmentNumber) {
+    public void findTotalSalaryInDepartment(Employee[] employees, int departmentNumber) {
         ArrayList<Employee> tmpList = new ArrayList<>();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getDepartmentNumber() == departmentNumber) {
@@ -108,7 +164,7 @@ public class DataWorking {
         System.out.println("Общие затраты на зарплату в отделе " + departmentNumber + " - " + sum + " руб.");
     }
 
-    public static void findAverageSalaryInDepartment(Employee[] employees, int departmentNumber) {
+    public void findAverageSalaryInDepartment(Employee[] employees, int departmentNumber) {
         ArrayList<Employee> tmpList = new ArrayList<>();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getDepartmentNumber() == departmentNumber) {
@@ -123,7 +179,7 @@ public class DataWorking {
         System.out.println("Средняя зарплата в отделе " + departmentNumber + " - " + average + " руб.");
     }
 
-    public static void indexSalaryInDepartment(Employee[] employees, int departmentNumber, int indexPercent) {
+    public void indexSalaryInDepartment(Employee[] employees, int departmentNumber, int indexPercent) {
         ArrayList<Employee> tmpList = new ArrayList<>();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getDepartmentNumber() == departmentNumber) {
@@ -137,7 +193,7 @@ public class DataWorking {
         }
     }
 
-    public static void printEmployeesOfDepartment(Employee[] employees, int departmentNumber) {
+    public void printEmployeesOfDepartment(Employee[] employees, int departmentNumber) {
         ArrayList<Employee> tmpList = new ArrayList<>();
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getDepartmentNumber() == departmentNumber) {
@@ -149,7 +205,7 @@ public class DataWorking {
         }
     }
 
-    public static void findSalaryMoreThanSalaryLimit(Employee[] employees, int salaryLimit) {
+    public void findSalaryMoreThanSalaryLimit(Employee[] employees, int salaryLimit) {
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getEmployeeSalary() > salaryLimit) {
                 System.out.println("Сотрудник, чья зарплата превышает установленный уровень - " + employees[i].getEmployeeFullName() + ", размер зарплаты - " + employees[i].getEmployeeSalary() + ", id = " + employees[i].getId());
@@ -157,7 +213,7 @@ public class DataWorking {
         }
     }
 
-    public static void findSalaryLessThanSalaryLimit(Employee[] employees, int salaryLimit) {
+    public void findSalaryLessThanSalaryLimit(Employee[] employees, int salaryLimit) {
         for (int i = 0; i < employees.length; i++) {
             if (employees[i].getEmployeeSalary() <= salaryLimit) {
                 System.out.println("Сотрудник, чья зарплата не превышает установленный уровень - " + employees[i].getEmployeeFullName() + ", размер зарплаты - " + employees[i].getEmployeeSalary() + ", id = " + employees[i].getId());
